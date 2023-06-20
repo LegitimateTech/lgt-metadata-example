822e54227dc79ff4372a3285b0bdf6289029c552
Daniel Duan <daniel@legitimate.tech>
2023-06-20 10:20:46 -0700

    feat: add animation url to generate script

--- generate.js
+++ generate.js
@@ -6,8 +6,9 @@ for (let i = 0; i <= NUMBER_OF_ITEMS; i++) {
 
   const json = {
     "name": "Unlocked Demo Token (Example)" + (i === 0 ? "" : ` ${i}/${NUMBER_OF_ITEMS}`),
-    "image": "https://ipfs.legitimate.tech/ipfs/QmZxHi87WSABAC2Sh4HWckVgTwXrW4GuVHF7f6LnssG5GU",
-    "description": `This NFT is for demo and testing purposes`
+    "image": "https://ipfs.legitimate.tech/ipfs/QmcBt413vhuETPgaXpKpU6X8puA1oLsr8mu426gdEUtHKG",
+    "description": `This NFT is for demo and testing purposes`,
+    "animation_url": "https://ipfs.legitimate.tech/ipfs/QmZXHCJJjZHfjVZbudmix8v1oLX7J3ARzSATAwcQeLp8wk",
   }
 
   if (i === 0) {
