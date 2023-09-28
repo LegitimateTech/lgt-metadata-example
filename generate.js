1c60363a08e334eeed4d5635d5e20dd9edad5e62
Daniel Duan <1474548+danielduan@users.noreply.github.com>
2023-09-28 10:04:35 +0000

    Update generate.js

--- generate.js
+++ generate.js
@@ -5,7 +5,7 @@ const NUMBER_OF_ITEMS = 100
 for (let i = 0; i <= NUMBER_OF_ITEMS; i++) {
 
   const json = {
-    "name": "Demo Token (Example)" + (i === 0 ? "" : ` ${i}/${NUMBER_OF_ITEMS}`),
+    "name": "Demo Token (Example)" + (i === 0 ? "" : ` #${i}/${NUMBER_OF_ITEMS}`),
     "image": "https://ipfs.legitimate.tech/ipfs/QmcBt413vhuETPgaXpKpU6X8puA1oLsr8mu426gdEUtHKG",
     "description": `This NFT is for demo and testing purposes`,
     "animation_url": "https://ipfs.legitimate.tech/ipfs/QmZXHCJJjZHfjVZbudmix8v1oLX7J3ARzSATAwcQeLp8wk",
